902972d0b184f5e4578af26884f85f95e2a1d087
Nathan Collier <nathaniel.collier@gmail.com>
2020-05-11 10:42:18 -0400

    fixed convert to use is_dimensionless, is_convertible returns true even if reciprocal is found

--- Variable.py
+++ Variable.py
@@ -2,92 +2,47 @@ from cf_units import Unit
 import xarray as xr
 import numpy as np
 
+"""
+
+"""
+
 @xr.register_dataarray_accessor('ilamb')
 class ilamb_variable:
     def __init__(self, xarray_obj):
         self._obj = xarray_obj
         self.measure = None
-        self.bounds = None
+        self.fraction = None
+        self.bounds = {}
         
     def convert(self,unit,density=998.2):
-        """Convert the variable to a given unit.
-
-        We use the UDUNITS2 library via the cf_units python interface
-        to convert the unit. Additional support is provided for unit
-        conversions in which substance information is required, such
-        as in precipitation where it is common to have data in a mass
-        rate per unit area [kg s-1 m-2] and desire it in a linear rate
-        [m s-1]. Conversion occurs in place, but also returns the
-        DataArray object so that the user can chain calls together.
-
-        Parameters
-        ----------
-        unit : str
-            the desired unit
-        density : float, optional
-            the mass density in [kg m-3] to use when converting,
-            defaults to that of water
-
-        Returns
-        -------
-        self : xarray.DataArray
+        """
+        - handles ( M L-2 T-1 ) --> ( L T-1 )
         """
         if 'units' not in self._obj.attrs:
             msg = "Cannot convert the units of a DataArray lacking the 'units' attribute"
             raise ValueError(msg)
         src_unit = Unit(self._obj.units)
-        tar_unit = Unit(unit)
-        
-        # Define some generic quantities
-        linear            = Unit("m")
-        linear_rate       = Unit("m s-1")
-        area_density      = Unit("kg m-2")
-        area_density_rate = Unit("kg m-2 s-1")
-        mass_density      = Unit("kg m-3")
-
-        # Do we need to multiply by density?
-        if ( (src_unit.is_convertible(linear_rate) and tar_unit.is_convertible(area_density_rate)) or
-             (src_unit.is_convertible(linear     ) and tar_unit.is_convertible(area_density     )) ):
-            with np.errstate(over='ignore',under='ignore'):
-                self._obj.data *= density
-            src_unit *= mass_density
-
-        # Do we need to divide by density?
-        if ( (tar_unit.is_convertible(linear_rate) and src_unit.is_convertible(area_density_rate)) or
-             (tar_unit.is_convertible(linear     ) and src_unit.is_convertible(area_density     )) ):
-            with np.errstate(over='ignore',under='ignore'):
-                self._obj.data /= density
+        tar_unit = Unit(unit)        
+        mass_density = Unit("kg m-3")
+        print("(%s) (%s) (%s)" % (src_unit,tar_unit,src_unit/tar_unit),((src_unit/tar_unit)/mass_density).is_dimensionless())        
+        if ((src_unit/tar_unit)/mass_density).is_dimensionless():
+            self._obj.data /= density
             src_unit /= mass_density
-
-        # Convert the unit
-        with np.errstate(over='ignore',under='ignore'):
-            src_unit.convert(self._obj.data,tar_unit,inplace=True)
+        elif ((tar_unit/src_unit)/mass_density).is_dimensionless():
+            self._obj.data *= density
+            src_unit *= mass_density
+        src_unit.convert(self._obj.data,tar_unit,inplace=True)
         self._obj.attrs['units'] = unit
         return self._obj
 
-    def add_measure(self,area_filename,fraction_filename=None):
-
-        # check that the measure is in the attributes
-        if 'cell_measures' not in self._obj.attrs:
-            msg = "DataArray does not contain the 'cell_measures' attribute"
-            raise ValueError(msg)
-
-        # try to get the cell areas from the file
-        measure_name = self._obj.cell_measures.split(":")[1].strip()
+    def addMeasure(self,area_filename,fraction_filename=None):
         with xr.open_dataset(area_filename) as ds:
-            if measure_name not in ds:
-                msg = "The cell_measures: %s is not found in %s" % (measure_name,area_filename)
+            measure_name = [v for v in ["areacella","area","areacello"] if v in ds]
+            if len(measure_name)==0:
+                msg = "Cannot find ['areacella','area','areacello'] in %s" % area_filename
                 raise ValueError(msg)
-            self.measure = ds[measure_name]
+            self.measure = ds[measure_name[0]].ilamb.convert("m2")
             
-        # optionally multiply by cell fractions
-        if fraction_filename is None: return
-        fraction_name = "sftlf"
-        with xr.open_dataset(fraction_filename) as ds:
-            if fraction_name not in ds:
-                msg = "The fraction variable sftlf is not found in %s" % (area_filename)
-                raise ValueError(msg)
-            self.measure *= ds[fraction_name].ilamb.convert("1")
 
     def integrate_space(self,mean=False):
 
@@ -181,24 +136,11 @@ if __name__ == "__main__":
                               "sftlf/sftlf_fx_CESM1-BGC_esmHistorical_r0i0p0.nc")
     
     ds = xr.open_dataset(nbp_file) # open the nbp
-    nbp = ds.nbp                   # pointer to the dataarray 
-    nbp.ilamb.add_bounds(ds)       # look in the dataset for 'bounds' on the time array
-    nbp.ilamb.add_measure(areacella_file,sftlf_file)  # give filenames in which we look for measures
-    mean = nbp.ilamb.integrate_space()  
-    mass = mean.ilamb.cumsum()
-    mass.ilamb.convert("Pg")
-    mass.plot()
-    plt.show()
-
-    """
-
-    Do you know if there is a way to get access to the parent dataset object in a dataarray?
-    You may be able to write it in a way that acts on the Dataset rather than the DataArray...?
+    nbp = ds.nbp                   # pointer to the dataarray
+    nbp.ilamb.addMeasure(areacella_file,sftlf_file)  # give filenames in which we look for measures
     
-
-    value = mean.ilamb.integrate_time(initial_time='1990-01-01',
-                                      final_time='1995-01-01',
-                                      mean=True)
-    value.ilamb.convert("Pg yr-1")
-    """
-
+    #mean = nbp.ilamb.integrate_space()
+    #mass = mean.ilamb.cumsum()
+    #mass.ilamb.convert("Pg")
+    #mass.plot()
+    #plt.show()
